ad68d002182ec43b1a39305a6702c69d48da94dd
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-02-23 19:24:12 +0100

    ADD filecompletion

--- gitlab-manager-completion.bash
+++ gitlab-manager-completion.bash
@@ -16,6 +16,38 @@
 # You should have received a copy of the GNU General Public License
 # along with gitlab-manager.  If not, see <http://www.gnu.org/licenses/>.
 
+function _fileCompletion() {
+    local cur
+    _init_completion || return
+    local IFS='
+' i j k
+    compopt -o filenames
+    if [[ -z ${CDPATH:-} || $cur == ?(.)?(.)/* ]]; then
+        _filedir
+        return
+    fi
+    local -r mark_dirs=$(_rl_enabled mark-directories && echo y)
+    local -r mark_symdirs=$(_rl_enabled mark-symlinked-directories && echo y)
+    for i in ${CDPATH//:/'
+'}; do
+        k="${#COMPREPLY[@]}"
+        for j in $(compgen -d -- "$i/$cur"); do
+            if [[ (-n $mark_symdirs && -L $j || -n $mark_dirs && ! -L $j) && ! -d ${j#$i/} ]]; then
+                j+="/"
+            fi
+            COMPREPLY[k++]=${j#$i/}
+        done
+    done
+    _filedir
+    if ((${#COMPREPLY[@]} == 1)); then
+        i=${COMPREPLY[0]}
+        if [[ $i == "$cur" && $i != "*/" ]]; then
+            COMPREPLY[0]="${i}/"
+        fi
+    fi
+    return
+}
+
 function genericCompletion() {
     mapfile -t COMPREPLY < <(compgen -W "$1" -- "${COMP_WORDS[COMP_CWORD]}")
 }
@@ -66,8 +98,12 @@ _gitlab-manager_completions() {
             listCompletion
             return
             ;;
-        -@(cf|-config-file|pf|-projects-file|gcf|-clone-folder|-from))
+        -@(gcf|-clone-folder|-from))
             _cd
+            return
+            ;;
+        -@(cf|-config-file|pf|-projects-file))
+            _fileCompletion
             return
             ;;
         --visibility)
